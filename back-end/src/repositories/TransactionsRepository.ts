d77514c7460d87ecf759b5cd444081d87833195b
Wellerson Prenholato <wellerson.prenholato@gmail.com>
2020-12-07 15:09:13 -0300

    Update TransactionsRepository.ts

--- back-end/src/repositories/TransactionsRepository.ts
+++ back-end/src/repositories/TransactionsRepository.ts
@@ -1,15 +1,6 @@
 import { EntityRepository, Repository } from 'typeorm';
-import csvParse from 'csv-parse';
-import fs from 'fs';
 
 import Transaction from '../models/Transaction';
-
-interface CSVArray {
-  title: string;
-  type: 'income' | 'outcome';
-  value: number;
-  category: string;
-}
 
 interface Balance {
   income: number;
@@ -22,57 +13,34 @@ class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
     const transactions = await this.find();
 
-    const transactionsIncome = transactions
-      .filter(transaction => transaction.type === 'income')
-      .reduce((totalIncome, transaction) => {
-        return totalIncome + transaction.value;
-      }, 0);
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += Number(transaction.value);
+            break;
 
-    const transactionsOutcome = transactions
-      .filter(transaction => transaction.type === 'outcome')
-      .reduce((totalOutcome, transaction) => {
-        return totalOutcome + transaction.value;
-      }, 0);
+          case 'outcome':
+            accumulator.outcome += Number(transaction.value);
+            break;
 
-    const balance = {
-      income: transactionsIncome,
-      outcome: transactionsOutcome,
-      total: transactionsIncome - transactionsOutcome,
-    };
+          default:
+            break;
+        }
 
-    return balance;
-  }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
 
-  public async loadCSV(filePath: string): Promise<CSVArray[]> {
-    const readCSVStream = fs.createReadStream(filePath);
+    const total = income - outcome;
 
-    const parseStream = csvParse({
-      from_line: 2,
-      ltrim: true,
-      rtrim: true,
-    });
-
-    const parseCSV = readCSVStream.pipe(parseStream);
-
-    const lines: CSVArray[] = [];
-
-    parseCSV.on('data', line => {
-      lines.push({
-        title: line[0],
-        type: line[1],
-        value: line[2],
-        category: line[3],
-      });
-    });
-
-    await new Promise(resolve => {
-      parseCSV.on('end', resolve);
-      console.log('cvs carregado!');
-    });
-
-    await fs.promises.unlink(filePath);
-
-    return lines;
+    return { income, outcome, total };
   }
 }
+
 export default TransactionsRepository;
